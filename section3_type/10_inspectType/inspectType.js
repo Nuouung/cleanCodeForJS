fbd816f180fa59c670ef90a516dcbea42b3975f1
Nuouung <jin6016@gmail.com>
2022-05-29 12:02:38 +0900

    commit

--- section3_type/10_inspectType/inspectType.js
+++ section3_type/10_inspectType/inspectType.js
@@ -1,4 +1,28 @@
 // typeof는 문자열을 반환한다
 // ex. typeof '문자열' -> 'string'
 
-// primitive vs reference
+// primitive vs reference <- 객체, 배열 등
+function myFunction() {}
+class MyClass {}
+
+console.log(typeof myFunction); // 'function'
+console.log(typeof MyClass); // 'function'
+// reference value는 typeof로 검출하기 어렵다
+// primitive는 잘 검출해줌
+
+console.log(typeof null); // object <- 자바스크립트 측에서 인정한 '오류'
+
+// 자바스크립트는 동적으로 변하는 언어 -> 타입도 변함
+
+// instanceof <- reference type을 검사할 때 쓰자
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const me = new Person('jinseok', 29);
+
+console.log(me instanceof Person); // true
+
+
---
+++ section3_type/11_undefinedAndNull/undefinedAndNull.js
@@ -0,0 +1,17 @@
+console.log(null);
+console.log(!null);
+console.log(!!null);
+
+console.log(null === false)
+console.log(!null === true)
+
+console.log(null + 123); // null = 0
+console.log(null * 123); // null = 0
+
+let variable;
+
+console.log(typeof variable);
+
+console.log(undefined + 123);
+console.log(!undefined);
+console.log(!undefined === !null);
